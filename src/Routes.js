e23df34d76ca1ab2b7ea701de96d3b5643d47c28
BogdanKaleniuk <bogdankaleniuk95@gmail.com>
2023-11-03 11:56:38 +0200

    done

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,37 @@
 import tabsData from "./tabs.json";
 import React, { useEffect, useState, Suspense } from "react";
-import { Route, Link, Routes, Outlet } from "react-router-dom";
+import { Route, Link, Routes } from "react-router-dom";
 
+// Імпортуйте ваші компоненти з відкладеним завантаженням за допомогою React.lazy()
 const DummyTable = React.lazy(() => import("./DummyTable"));
 const DummyChart = React.lazy(() => import("./DummyChart"));
 const DummyList = React.lazy(() => import("./DummyList"));
 
 const TabsComponent = () => {
   const [tabs, setTabs] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setTabs(tabsData);
   }, []);
+
+  const handleTabChange = async () => {
+    console.log("sds");
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 200);
+  };
 
   return (
     <div>
       <ul>
         {tabs.map((tab) => (
           <li key={tab.id}>
-            <Link to={`/${tab.id}`}>{tab.title}</Link>
+            <Link to={`/${tab.id}`} onClick={handleTabChange}>
+              {tab.title}
+            </Link>
           </li>
         ))}
       </ul>
@@ -29,7 +42,7 @@ const TabsComponent = () => {
             key={tab.id}
             path={`/${tab.id}`}
             element={
-              <Suspense fallback={<div>Loading...</div>}>
+              <Suspense fallback={<div>{loading ? "Loading..." : null}</div>}>
                 {tab.id === "dummyTable" ? (
                   <DummyTable title={tab.title} order={tab.order} />
                 ) : tab.id === "dummyChart" ? (
